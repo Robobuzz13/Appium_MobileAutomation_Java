914815d484a040f24075358ead41fd7905290f27
Susilkumar Udhayakumar <87655701+Robobuzz13@users.noreply.github.com>
2024-09-03 19:54:11 +0530

    Protocol design initial script
    Initial script setup for socket connection and console connection

---
+++ src/main/java/test/automation/networkProtocol/consoleConnection.java
@@ -0,0 +1,65 @@
+package test.automation.networkProtocol;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class consoleConnection {
+
+    public void waitForConsoleMessage(BufferedReader in, String expectedMessage) throws IOException {
+        String line;
+        while ((line = in.readLine()) != null) {
+            if (line.equals(expectedMessage)) {
+                break;
+            }
+        }
+    }
+
+    public List<String> returnListOfConsoleMessage(BufferedReader in, String expectedMessage) throws IOException {
+        List<String> consoleMsgLst = new ArrayList<>();
+        String line;
+        while ((line = in.readLine()) != null) {
+            if (line.equals(expectedMessage)) {
+                consoleMsgLst.add(line);
+            }
+        }
+        return consoleMsgLst;
+    }
+
+    public List<String> returnListOfConsoleMessage(BufferedReader in) throws IOException {
+        List<String> consoleMsgLst = new ArrayList<>();
+        String line;
+        while ((line = in.readLine()) != null) {
+            consoleMsgLst.add(line);
+        }
+        return consoleMsgLst;
+    }
+
+    public List<String> executeCommandInConsole(String[] command,String expectedMessage){
+        List<String> returnStringStatement = new ArrayList<>();
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder(command);
+            processBuilder.redirectErrorStream(true);
+            Process process = processBuilder.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            returnStringStatement.addAll(returnListOfConsoleMessage(reader,expectedMessage));
+        }  catch (IOException _) {   // exception handling will be done later
+        }
+        return returnStringStatement;
+    }
+
+    public List<String> executeCommandInConsole(String[] command){
+        List<String> returnStringStatement = new ArrayList<>();
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder(command);
+            processBuilder.redirectErrorStream(true);
+            Process process = processBuilder.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            returnStringStatement.addAll(returnListOfConsoleMessage(reader));
+        }  catch (IOException _) {   // exception handling will be done later
+        }
+        return returnStringStatement;
+    }
+}
